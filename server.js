98148bdb0d8c3689619b57ad299b2a1b95b01317
lotenka <makmartynyuk@gmail.com>
2026-04-26 17:29:55 +0300

    Initial commit (smart home server)

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const mqtt = require("mqtt");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const mqttClient = mqtt.connect("mqtt://broker.hivemq.com");
+
+// MQTT подписки
+mqttClient.on("connect", () => {
+    console.log("MQTT connected");
+    mqttClient.subscribe("home/#");
+});
+
+// MQTT → Web
+mqttClient.on("message", (topic, message) => {
+    const msg = message.toString();
+    io.emit("mqtt", { topic, msg });
+});
+
+// Web → MQTT
+io.on("connection", (socket) => {
+    socket.on("command", ({ topic, value }) => {
+        mqttClient.publish(topic, value);
+    });
+});
+
+app.use(express.static("public"));
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
